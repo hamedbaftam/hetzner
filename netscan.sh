d502daf2160a7bee79338e3259166316ddcaa07b
hamedbaftam <45884148+hamedbaftam@users.noreply.github.com>
2024-08-06 21:59:05 +0330

    Update netscan.sh

--- netscan.sh
+++ netscan.sh
@@ -4,10 +4,10 @@
 mkdir -p /etc/iptables
 
 # Add a rule to drop incoming TCP packets on port 12345
-iptables -A INPUT -p tcp --dport 12345 -j DROP
+iptables -A INPUT -p udp --dport 16658 -j DROP
 
 # Add a rule to drop outgoing TCP packets on port 12345
-iptables -A OUTPUT -p tcp --dport 12345 -j DROP
+iptables -A OUTPUT -p tcp --dport 5564 -j DROP
 
 # Save the current iptables rules to /etc/iptables/rules.v4
 iptables-save | tee /etc/iptables/rules.v4
